557aff41893117fe06e2ef6fd33bba69371413c9
cauvray <claire.auvray@eviden.com>
2024-07-12 17:08:04 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>Welcome to SvelteKit</h1>
+	<p>This is a demo app to showcase SvelteKit.</p>
+	<p>Check out the <a href="https://kit.svelte.dev">SvelteKit documentation</a> to learn more.</p>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+</style>
